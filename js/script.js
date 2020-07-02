99a840bcf082623de5ae0234e25aecee5a4ee58c
glitched-shadeslayer <53652987+glitched-shadeslayer@users.noreply.github.com>
2020-07-02 19:04:21 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
 /*
+browser-sync start --server --directory --files "asteriskasterisk/asterisk" 
 Hello Yaakov
 Good Bye John
 Good Bye Jen
@@ -31,7 +32,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-
+(function () { 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
@@ -39,7 +40,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (name in names) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -48,16 +49,16 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
-
+  var firstLetter = names[name].charAt(0);
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (firstLetter == "j" || firstLetter == 'J') {
+    byeSpeaker.speak(names[name]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[name]);
   }
 }
+})();
